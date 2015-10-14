JamesMarcil/Asteroids

commit 145bf477b65dbeb192dc65c4c8b61e33918805af

Implemented StateMachine.
Created the StateMachine<T> class to manage transitioning between GameStates.
StateMachine<T> maps an enum T to a class implementing the IGameState interface.
The IGameState interface defines Enter, Update, Render, and Exit methods.
Renamed MyDemoGame to GameState, now inherits from IGameState instead of
DirectXGameCore. Added two additional dummy states, MenuState and ExitState with
black and white backgrounds respectively. Moved the WinMain entrance point into
its own separate file in main.cpp. GameStates.h header conatains the enum
enumerating the current GameStates. You can switch between the three states
using the 1,2,3 keys on the keyboard. StateMachine<T> is now hooked into the
DirectXGameCore::Run method. Add States filter to Visual Studio to contain any
IGameState classes.

DirectX11_Starter/ExitState.h
@@ -1,0 +1,30 @@
+#ifndef EXIT_STATE_H
+#define EXIT_STATE_H
+
+// State
+#include "IGameState.h"
+
+// DirectX
+#include <d3d11.h>
+
+class ExitState : public IGameState
+{
+private:
+    ID3D11Device*           const m_pDevice;
+    ID3D11DeviceContext*    const m_pDeviceContext;
+    IDXGISwapChain*         const m_pSwapChain;
+    ID3D11RenderTargetView* const m_pRenderTargetView;
+    ID3D11DepthStencilView* const m_pDepthStencilView;
+
+    virtual void Enter( void ) override;
+    virtual void Update( float deltaTime, float totalTime ) override;
+    virtual void Render( float deltaTime, float totalTime ) override;
+    virtual void Exit( void ) override;
+    
+public:
+    ExitState( ID3D11Device* const pDevice, ID3D11DeviceContext* const pDeviceContext, IDXGISwapChain* pSwapChain, ID3D11RenderTargetView* const pRenderTargetView, ID3D11DepthStencilView* const pDepthStencilView )
+        : m_pDevice( pDevice ), m_pDeviceContext( pDeviceContext ), m_pSwapChain( pSwapChain ), m_pRenderTargetView( pRenderTargetView ), m_pDepthStencilView( pDepthStencilView )
+    {}
+};
+
+#endif

DirectX11_Starter/IGameState.h
@@ -1,0 +1,15 @@
+#ifndef IGAMESTATE_H
+#define IGAMESTATE_H
+
+class IGameState
+{
+    template <typename T>
+    friend class StateMachine;
+
+    virtual void Enter( void ) = 0;
+    virtual void Update( float deltaTime, float totalTime ) = 0;
+    virtual void Render( float deltaTime, float totalTime ) = 0;
+    virtual void Exit( void ) = 0;
+};
+
+#endif

DirectX11_Starter/MenuState.h
@@ -1,0 +1,30 @@
+#ifndef MENU_STATE_H
+#define MENU_STATE_H
+
+// IGameState interface
+#include "IGameState.h"
+
+// DirectX
+#include <d3d11.h>
+
+class MenuState : public IGameState
+{
+public:
+    MenuState( ID3D11Device* const pDevice, ID3D11DeviceContext* const pDeviceContext, IDXGISwapChain* pSwapChain, ID3D11RenderTargetView* const pRenderTargetView, ID3D11DepthStencilView* const pDepthStencilView )
+        : m_pDevice( pDevice ), m_pDeviceContext( pDeviceContext ), m_pSwapChain( pSwapChain ), m_pRenderTargetView( pRenderTargetView ), m_pDepthStencilView( pDepthStencilView )
+    {}
+
+private:
+    ID3D11Device*           const m_pDevice;
+    ID3D11DeviceContext*    const m_pDeviceContext;
+    IDXGISwapChain*         const m_pSwapChain;
+    ID3D11RenderTargetView* const m_pRenderTargetView;
+    ID3D11DepthStencilView* const m_pDepthStencilView;
+
+    virtual void Enter( void ) override;
+    virtual void Update( float deltaTime, float totalTime ) override;
+    virtual void Render( float deltaTime, float totalTime ) override;
+    virtual void Exit( void ) override;
+};
+
+#endif

DirectX11_Starter/DirectX11_Starter.vcxproj
@@ -76,11 +76,14 @@
     <ClCompile Include="Camera.cpp" />
     <ClCompile Include="CameraManager.cpp" />
     <ClCompile Include="DebugCamera.cpp" />
+    <ClCompile Include="ExitState.cpp" />
     <ClCompile Include="GameEntity.cpp" />
+    <ClCompile Include="GameState.cpp" />
     <ClCompile Include="InputManager.cpp" />
+    <ClCompile Include="main.cpp" />
     <ClCompile Include="Material.cpp" />
+    <ClCompile Include="MenuState.cpp" />
     <ClCompile Include="Mesh.cpp" />
-    <ClCompile Include="MyDemoGame.cpp" />
     <ClCompile Include="dxerr.cpp" />
     <ClCompile Include="DirectXGameCore.cpp" />
     <ClCompile Include="ResourceManager.cpp" />
@@ -92,16 +95,21 @@
     <ClInclude Include="CameraManager.h" />
     <ClInclude Include="DebugCamera.h" />
     <ClInclude Include="DXMacros.h" />
+    <ClInclude Include="ExitState.h" />
     <ClInclude Include="GameEntity.h" />
+    <ClInclude Include="GameState.h" />
+    <ClInclude Include="GameStates.h" />
+    <ClInclude Include="IGameState.h" />
     <ClInclude Include="InputManager.h" />
     <ClInclude Include="Keycodes.h" />
     <ClInclude Include="Material.h" />
+    <ClInclude Include="MenuState.h" />
     <ClInclude Include="Mesh.h" />
-    <ClInclude Include="MyDemoGame.h" />
     <ClInclude Include="dxerr.h" />
     <ClInclude Include="DirectXGameCore.h" />
     <ClInclude Include="ResourceManager.h" />
     <ClInclude Include="SimpleShader.h" />
+    <ClInclude Include="StateMachine.h" />
     <ClInclude Include="Transform.h" />
     <ClInclude Include="Vertex.h" />
   </ItemGroup>

DirectX11_Starter/DirectX11_Starter.vcxproj.filters
@@ -34,12 +34,15 @@
     <Filter Include="Header Files\Graphics">
       <UniqueIdentifier>{e9bc017b-34fa-43fe-a9f0-e1b806fb5ee1}</UniqueIdentifier>
     </Filter>
+    <Filter Include="Header Files\States">
+      <UniqueIdentifier>{e6c6f851-32ac-42ba-a41e-f21544ac4fb8}</UniqueIdentifier>
+    </Filter>
+    <Filter Include="Source Files\States">
+      <UniqueIdentifier>{2f3ec692-7737-45c6-9d7e-a25bed35dd72}</UniqueIdentifier>
+    </Filter>
   </ItemGroup>
   <ItemGroup>
     <ClCompile Include="dxerr.cpp">
-      <Filter>Source Files</Filter>
-    </ClCompile>
-    <ClCompile Include="MyDemoGame.cpp">
       <Filter>Source Files</Filter>
     </ClCompile>
     <ClCompile Include="DirectXGameCore.cpp">
@@ -75,12 +78,21 @@
     <ClCompile Include="DebugCamera.cpp">
       <Filter>Source Files\Game</Filter>
     </ClCompile>
+    <ClCompile Include="MenuState.cpp">
+      <Filter>Source Files\States</Filter>
+    </ClCompile>
+    <ClCompile Include="main.cpp">
+      <Filter>Source Files</Filter>
+    </ClCompile>
+    <ClCompile Include="GameState.cpp">
+      <Filter>Source Files\States</Filter>
+    </ClCompile>
+    <ClCompile Include="ExitState.cpp">
+      <Filter>Source Files\States</Filter>
+    </ClCompile>
   </ItemGroup>
   <ItemGroup>
     <ClInclude Include="dxerr.h">
-      <Filter>Header Files</Filter>
-    </ClInclude>
-    <ClInclude Include="MyDemoGame.h">
       <Filter>Header Files</Filter>
     </ClInclude>
     <ClInclude Include="DirectXGameCore.h">
@@ -124,6 +136,24 @@
     </ClInclude>
     <ClInclude Include="DebugCamera.h">
       <Filter>Header Files\Game</Filter>
+    </ClInclude>
+    <ClInclude Include="StateMachine.h">
+      <Filter>Header Files\Managers</Filter>
+    </ClInclude>
+    <ClInclude Include="MenuState.h">
+      <Filter>Header Files\States</Filter>
+    </ClInclude>
+    <ClInclude Include="GameStates.h">
+      <Filter>Header Files\States</Filter>
+    </ClInclude>
+    <ClInclude Include="IGameState.h">
+      <Filter>Header Files\States</Filter>
+    </ClInclude>
+    <ClInclude Include="GameState.h">
+      <Filter>Header Files\States</Filter>
+    </ClInclude>
+    <ClInclude Include="ExitState.h">
+      <Filter>Header Files\States</Filter>
     </ClInclude>
   </ItemGroup>
   <ItemGroup>

DirectX11_Starter/DirectXGameCore.cpp
@@ -10,10 +10,23 @@
 // -------------------------------------------------------------
 
 #include "DirectXGameCore.h"
+
+// Windows
 #include <WindowsX.h>
+
+// STD
 #include <sstream>
+
+// Managers
 #include "InputManager.h"
 #include "CameraManager.h"
+
+// State
+#include "StateMachine.h"
+#include "GameStates.h"
+#include "MenuState.h"
+#include "GameState.h"
+#include "ExitState.h"
 
 #pragma region Global Window Callback
 
@@ -42,7 +55,7 @@ LRESULT CALLBACK MainWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 // --------------------------------------------------------
 DirectXGameCore::DirectXGameCore(HINSTANCE hInstance)
 	: hAppInst(hInstance),
-	windowCaption(L"DirectX Game"),
+	windowCaption(L"Galactic Bulge presents: Asteroids"),
 	windowWidth(800),
 	windowHeight(600),
 	hMainWnd(0),
@@ -115,6 +128,13 @@ bool DirectXGameCore::Init()
 	// DirectX (specifically Direct3D)
 	if(!InitDirect3D())
 		return false;
+
+	// Register Game States
+    StateMachine<GameStates>* pState = StateMachine<GameStates>::instance();
+    pState->RegisterState<MenuState>( GameStates::MENU, device, deviceContext, swapChain, renderTargetView, depthStencilView );
+    pState->RegisterState<GameState>( GameStates::GAME, device, deviceContext, swapChain, renderTargetView, depthStencilView );
+    pState->RegisterState<ExitState>( GameStates::EXIT, device, deviceContext, swapChain, renderTargetView, depthStencilView );
+    pState->GoToState( GameStates::MENU );
 
 	// Everything was set up properly
 	return true;
@@ -351,10 +371,12 @@ int DirectXGameCore::Run()
 
 			// Standard game loop type stuff
 			CalculateFrameStats();
+
 			InputManager::instance()->Update( deltaTime );
+
             CameraManager::instance()->Update( deltaTime );
-			UpdateScene(deltaTime, totalTime);
-			DrawScene(deltaTime, totalTime);			
+
+            StateMachine<GameStates>::instance()->Update( deltaTime, totalTime );
 		}
 	}
 

DirectX11_Starter/DirectXGameCore.h
@@ -32,8 +32,6 @@ public:
 	// derived classes to implement custom functionality
 	virtual bool Init();
 	virtual void OnResize(); 
-	virtual void UpdateScene(float deltaTime, float totalTime) = 0;
-	virtual void DrawScene(float deltaTime, float totalTime)   = 0;
 	virtual LRESULT ProcessMessage(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 protected:

DirectX11_Starter/ExitState.cpp
@@ -1,0 +1,46 @@
+#include "ExitState.h"
+
+// DirectX
+#include "DXMacros.h"
+
+// Managers
+#include "InputManager.h"
+
+// State
+#include "StateMachine.h"
+#include "GameStates.h"
+
+void ExitState::Enter( void )
+{
+	/* Nothing to do. */
+}
+
+void ExitState::Update( float deltaTime, float totalTime )
+{
+    // Check input
+    InputManager* pInput = InputManager::instance();
+    StateMachine<GameStates>* pState = StateMachine<GameStates>::instance();
+
+    if( pInput->IsKeyDown( '1' ) )
+    {
+        pState->GoToState( GameStates::MENU );
+    }
+    else if( pInput->IsKeyDown( '2' ) )
+    {
+        pState->GoToState( GameStates::GAME );
+    }
+}
+
+void ExitState::Render( float deltaTime, float totalTime )
+{
+    // Render the screen white
+	const float white[4] = { 1.0f, 1.0f, 1.0f, 1.0f };
+	m_pDeviceContext->ClearRenderTargetView( m_pRenderTargetView, white );
+	m_pDeviceContext->ClearDepthStencilView( m_pDepthStencilView, D3D11_CLEAR_DEPTH | D3D11_CLEAR_STENCIL, 1.0f, 0 );
+    HR( m_pSwapChain->Present( 0, 0 ) ); 
+}
+
+void ExitState::Exit( void )
+{
+	/* Nothing to do. */
+}

commit aea686388b679932875d44a7275b20f9c919862d

Fixing memory leak introduced in commit 145bf47.
Added virtual destructor to IGameState interface so that the destructor would be
invoked for child classes.

DirectX11_Starter/ExitState.h
@@ -25,6 +25,8 @@ public:
     ExitState( ID3D11Device* const pDevice, ID3D11DeviceContext* const pDeviceContext, IDXGISwapChain* pSwapChain, ID3D11RenderTargetView* const pRenderTargetView, ID3D11DepthStencilView* const pDepthStencilView )
         : m_pDevice( pDevice ), m_pDeviceContext( pDeviceContext ), m_pSwapChain( pSwapChain ), m_pRenderTargetView( pRenderTargetView ), m_pDepthStencilView( pDepthStencilView )
     {}
+
+	virtual ~ExitState() {}
 };
 
 #endif

DirectX11_Starter/IGameState.h
@@ -3,6 +3,7 @@
 
 class IGameState
 {
+private:
     template <typename T>
     friend class StateMachine;
 
@@ -10,6 +11,8 @@ class IGameState
     virtual void Update( float deltaTime, float totalTime ) = 0;
     virtual void Render( float deltaTime, float totalTime ) = 0;
     virtual void Exit( void ) = 0;
+public:
+    virtual ~IGameState() {}
 };
 
 #endif

DirectX11_Starter/MenuState.h
@@ -14,6 +14,7 @@ public:
         : m_pDevice( pDevice ), m_pDeviceContext( pDeviceContext ), m_pSwapChain( pSwapChain ), m_pRenderTargetView( pRenderTargetView ), m_pDepthStencilView( pDepthStencilView )
     {}
 
+	virtual ~MenuState() {}
 private:
     ID3D11Device*           const m_pDevice;
     ID3D11DeviceContext*    const m_pDeviceContext;