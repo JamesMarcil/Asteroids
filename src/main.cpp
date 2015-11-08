62a8fbf87342bd14dfa96f0f9f80617f769a2587
JamesMarcil <JamesMarcil@users.noreply.github.com>
2015-11-08 18:36:06 -0500

    Adding a debug console.
    Debug builds now have access to a console to print debug information. See
    comment in main.cpp for a link to the StackOverflow post the code is based on.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,10 @@
+// Windows
 #include <Windows.h>
+
+// STD
+#include <stdio.h>
+#include <iostream>
+
 #include "DirectXGameCore.h"
 
 // Include run-time memory checking in debug builds, so 
@@ -8,15 +14,49 @@
 #include <crtdbg.h>
 #endif
 
+/*
+ * This function will create a console to print debug information.
+ * Based on the following StackOverflow post:
+ *      http://stackoverflow.com/questions/311955/redirecting-cout-to-a-console-in-windows
+ */
+void CreateDebugConsole(void)
+{
+    // Allocate a console for this application.
+    AllocConsole();
+    SetConsoleTitle(L"Asteroids Debug Console");
+
+    // Redirect input/output to the console.
+    FILE *pIn, *pErr, *pOut;
+    freopen_s(&pIn, "CONIN$", "r", stdin);
+    freopen_s(&pOut, "CONOUT$", "w", stdout);
+    freopen_s(&pErr, "CONOUT$", "w", stderr);
+
+    // Clear the error state of the std::iostream objects.
+    std::cout.clear();
+    std::cerr.clear();
+    std::cin.clear();
+    std::wcout.clear();
+    std::wcerr.clear();
+    std::wcin.clear();
+
+	HANDLE h = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_SCREEN_BUFFER_INFO coninfo;
+	GetConsoleScreenBufferInfo(h, &coninfo);
+	coninfo.dwSize.Y = 500;                         // Maximum mumber of lines for the output console.
+	SetConsoleScreenBufferSize(h, coninfo.dwSize);
+}
+
 // --------------------------------------------------------
 // Win32 Entry Point - Where your program starts
 // --------------------------------------------------------
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
 				   PSTR cmdLine, int showCmd)
 {
-	// Enable run-time memory check for debug builds.
 #if defined(DEBUG) | defined(_DEBUG)
+	// Enable run-time memory check for debug builds.
 	_CrtSetDbgFlag( _CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF );
+
+    CreateDebugConsole();
 #endif
 
 	// Create the game object.
@@ -26,7 +66,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE prevInstance,
 	// set up geometry and shaders, etc.
 	if( !game.Init() )
 		return 0;
-	
+
 	// All set to run the game loop
 	return game.Run();
 }
