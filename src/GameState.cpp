JamesMarcil/Asteroids

commit 4f2dae8817fdd53b86e6c37cbae765db0141f6cd

MenuState is now no longer a simple blank screen.

src/GameState.cpp
@@ -30,8 +30,6 @@
 #include "UIRenderSystem.h"
 #include "UIUpdateSystem.h"
 
-#include "UITextComponent.h"
-
 // For the DirectX Math library
 using namespace DirectX;
 
@@ -41,7 +39,7 @@ using namespace DirectX;
 // Initializes the base class (including the window and D3D),
 // sets up our geometry and loads the shaders (among other things)
 // --------------------------------------------------------
-void GameState::Enter( void )
+void GameState::Enter(void)
 {
     // Register resources with the ResourceManager
     if(!isInitialized)
@@ -61,15 +59,15 @@ void GameState::Enter( void )
 
         // Register Systems.
         EntityManager* pEntity = EntityManager::Instance();
-        pEntity->AddSystem<PhysicsSystem>();
-		pEntity->AddSystem<InputControllerSystem>();
-        pEntity->AddSystem<UIUpdateSystem>();
-        pEntity->AddSystem<ClearSystem>();
-        pEntity->AddSystem<RenderSystem>();
-        pEntity->AddSystem<SkyboxSystem>();
-        pEntity->AddSystem<UIRenderSystem>();
-		pEntity->AddSystem<SwapSystem>();
-		pEntity->AddSystem<ScriptSystem>();
+        pEntity->AddSystemWithPriority<PhysicsSystem, 0>();
+		pEntity->AddSystemWithPriority<InputControllerSystem, 1>();
+        pEntity->AddSystemWithPriority<UIUpdateSystem, 2>();
+        pEntity->AddSystemWithPriority<ClearSystem, 3>();
+        pEntity->AddSystemWithPriority<RenderSystem, 4>();
+        pEntity->AddSystemWithPriority<SkyboxSystem, 5>();
+        pEntity->AddSystemWithPriority<UIRenderSystem, 6>();
+		pEntity->AddSystemWithPriority<SwapSystem, 7>();
+		pEntity->AddSystemWithPriority<ScriptSystem, 8>();
 
         // Make a SpotLight
         GameEntity spotlight = EntityFactory::CreateSpotlight
@@ -79,17 +77,10 @@ void GameState::Enter( void )
             XMFLOAT3(0.0f, 0.0f, 1.0f),       // Direction
             30.0f,                            // Specular Exponent
             1.0f                              // SpotLight Power
-        );
+       );
 
 		//Make Player
 		GameEntity player = EntityFactory::CreatePlayer(XMFLOAT3(0.0f, 0.0f, 0.0f));
-
-        // Make Button.
-        GameEntity button = EntityFactory::CreateButton(XMFLOAT2(0.0f, 0.0f), 100.0f, 100.0f, "UIMaterial");
-
-        // Make Text.
-        GameEntity text = pEntity->Create();
-        pEntity->AddComponent<UITextComponent>(text, L"Test", "TimesNewRoman", XMFLOAT2{400.0f, 300.0f}, XMFLOAT4{0.0f, 0.0f, 0.0f, 1.0f});
 
         isInitialized = true;
     }
@@ -132,27 +123,15 @@ void GameState::EventRouter(const std::string& name, void* data)
 	}
 }
 
-
-
-#pragma region Game Loop
 // --------------------------------------------------------
 // Update your game here - take input, move objects, etc.
 // --------------------------------------------------------
-void GameState::Update( float deltaTime, float totalTime )
+void GameState::Update(float deltaTime, float totalTime)
 {
-	// Handle Input
-    InputManager* pInput = InputManager::Instance();
-    StateMachine<GameStates>* pState = StateMachine<GameStates>::Instance();
-
-    if( pInput->IsKeyDown( '1' ) )
-    {
-        pState->GoToState( GameStates::MENU );
-    }
-	else if (pInput->IsKeyDown('3'))
-	{
-		pState->GoToState(GameStates::EXIT);
-	}
+    /* Nothing to do. */
 }
 
-void GameState::Exit( void ) { /* Nothing to do. */ }
-#pragma endregion
+void GameState::Exit(void) 
+{ 
+    /* Nothing to do. */ 
+}

include/EntityFactory.h
@@ -22,7 +22,7 @@ private:
 
 public:
     static GameEntity CreateTextField(std::wstring text, std::string spritefont_id, DirectX::XMFLOAT2 position, DirectX::XMFLOAT4 color);
-    static GameEntity CreateButton(DirectX::XMFLOAT2 center, float width, float height, std::string material_id);
+    static GameEntity CreateButton(DirectX::XMFLOAT2 center, float width, float height, std::string event, std::wstring text, std::string spritefont_id, DirectX::XMFLOAT2 position, DirectX::XMFLOAT4 color);
     static GameEntity CreateAsteroid(DirectX::XMFLOAT3 position, DirectX::XMFLOAT3 velocity, DirectX::XMFLOAT3 acceleration);
     static GameEntity CreatePlayer(DirectX::XMFLOAT3 position);
     static GameEntity CreateDirectionalLight(DirectX::XMFLOAT4 color, DirectX::XMFLOAT3 direction, float specularExponent);

include/MenuState.h
@@ -1,20 +1,23 @@
 #ifndef MENU_STATE_H
 #define MENU_STATE_H
 
-// IGameState interface
+// Interfaces
 #include "IGameState.h"
+#include "IEventListener.h"
 
-// DirectX
-#include <d3d11.h>
+// STD
+#include <string>
 
-class MenuState : public IGameState
+class MenuState : public IGameState, public IEventListener
 {
 public:
-	virtual ~MenuState() {}
+	virtual ~MenuState(void) override;
+
 private:
-    virtual void Enter( void ) override;
-    virtual void Update( float deltaTime, float totalTime ) override;
-    virtual void Exit( void ) override;
+    virtual void EventRouter(const std::string& event, void* pData) override;
+    virtual void Enter(void) override;
+    virtual void Update(float deltaTime, float totalTime) override;
+    virtual void Exit(void) override;
 };
 
 #endif

include/SkyboxSystem.h
@@ -12,6 +12,7 @@
 
 // Forward Declarations
 class ResourceManager;
+class EventManager;
 
 class SkyboxSystem : public System<SkyboxSystem>, public IEventListener
 {
@@ -22,6 +23,7 @@ public:
 	virtual void EventRouter(const std::string& name, void* data) override;
 private:
 	ResourceManager* m_pResource;
+    EventManager* m_pEvent;
 	std::string textureNames[3];
 	int currentTexture;
 	bool warping;

src/EntityFactory.cpp
@@ -32,15 +32,15 @@ GameEntity EntityFactory::CreateTextField(std::wstring text, std::string spritef
     return textfield;
 }
 
-GameEntity EntityFactory::CreateButton(DirectX::XMFLOAT2 center, float width, float height, std::string material_id)
+GameEntity EntityFactory::CreateButton(DirectX::XMFLOAT2 center, float width, float height, std::string event, std::wstring text, std::string spritefont_id, DirectX::XMFLOAT2 position, DirectX::XMFLOAT4 color)
 {
     EntityManager* pEntity = EntityManager::Instance();
     ResourceManager* pResource = ResourceManager::Instance();
 
     GameEntity button = pEntity->Create();
-    pEntity->AddComponent<ButtonComponent>(button, true, "PlayClicked");
+    pEntity->AddComponent<ButtonComponent>(button, true, event);
     pEntity->AddComponent<AABBComponent>(button, XMFLOAT2{center.x + 400.0f, center.y + 300.0f}, width / 2.0f, height / 2.0f); // TODO: Replace with half window width and half window height
-    pEntity->AddComponent<UIRenderComponent>(button, center, width, height, pResource->GetMaterial(material_id));
+    pEntity->AddComponent<UITextComponent>(button, text, spritefont_id, position, color);
 
     return button;
 }

src/MenuState.cpp
@@ -1,37 +1,115 @@
 #include "MenuState.h"
 
 // DirectX
-#include <d3d11.h>
-#include "DXMacros.h"
+#include <DirectXMath.h>
 
 // Managers
-#include "InputManager.h"
+#include "CameraManager.h"
+#include "ResourceManager.h"
+#include "EntityManager.h"
+#include "EventManager.h"
+
+// Systems
+#include "ClearSystem.h"
+#include "UIUpdateSystem.h"
+#include "UIRenderSystem.h"
+#include "SkyboxSystem.h"
+#include "SwapSystem.h"
 
 // State
 #include "StateMachine.h"
 #include "GameStates.h"
 
-void MenuState::Enter( void )
+#include "EntityFactory.h"
+
+#include "StaticCamera.h"
+
+using namespace DirectX;
+
+#pragma region Destructor
+
+MenuState::~MenuState(void)
 {
-	/* Nothing to do. */
+    /* Nothing to do. */
 }
 
-void MenuState::Update( float deltaTime, float totalTime )
+#pragma endregion
+
+#pragma region IGameState Interface
+
+void MenuState::Enter(void)
 {
-    // Check input
-    InputManager* pInput = InputManager::Instance();
-    StateMachine<GameStates>* pState = StateMachine<GameStates>::Instance();
-    if( pInput->IsKeyDown( '2' ) )
-    {
-        pState->GoToState( GameStates::GAME );
-    }
-    else if( pInput->IsKeyDown( '3' ) )
-    {
-        pState->GoToState( GameStates::EXIT );
-    }
+    ResourceManager* pResource = ResourceManager::Instance();
+    pResource->ParseJSONFile("json/resources.json");
+
+    EventManager* pEvent = EventManager::Instance();
+    pEvent->Register("PlayClicked", this);
+    pEvent->Register("WarpBegin", this);
+    pEvent->Register("WarpEnd", this);
+
+    CameraManager* pCamera = CameraManager::Instance();
+    pCamera->RegisterCamera<StaticCamera>("Menu_Camera", 0.0f, 0.0f, -5.0f);
+    pCamera->SetActiveCamera("Menu_Camera");
+
+    EntityManager* pEntity = EntityManager::Instance();
+    pEntity->AddSystem<UIUpdateSystem>();
+    pEntity->AddSystem<ClearSystem>();
+    pEntity->AddSystem<SkyboxSystem>();
+    pEntity->AddSystem<UIRenderSystem>();
+    pEntity->AddSystem<SwapSystem>();
+
+    GameEntity title = EntityFactory::CreateTextField
+        (
+            L"Asteroids",
+            "SpaceAge54",
+            XMFLOAT2{125.0f, 0.0f},
+            XMFLOAT4{1.0f, 1.0f, 1.0f, 1.0f}
+        );
+
+    GameEntity button = EntityFactory::CreateButton
+        (
+            XMFLOAT2{0.0f, 0.0f},
+            800,
+            600,
+            "PlayClicked",
+            L"Click to Play",
+            "SpaceAge48",
+            XMFLOAT2{75.0f, 275.0f},
+            XMFLOAT4{1.0f, 1.0f, 1.0f, 1.0f}
+        );
+
+    pEvent->Fire("WarpBegin", nullptr);
 }
 
-void MenuState::Exit( void )
+void MenuState::Update(float deltaTime, float totalTime)
 {
-	/* Nothing to do. */
+    /* Nothing to do. */
+}
+
+void MenuState::Exit(void)
+{
+    EntityManager* pEntity = EntityManager::Instance();
+    pEntity->Clear();
+    pEntity->ClearSystems();
+
+    EventManager* pEvent = EventManager::Instance();
+    pEvent->UnRegister("WarpBegin", this);
+    pEvent->UnRegister("WarpEnd", this);
+    pEvent->UnRegister("PlayClicked", this);
+}
+
+#pragma endregion
+
+void MenuState::EventRouter(const std::string& event, void* pData)
+{
+    if(event == "PlayClicked")
+    {
+        StateMachine<GameStates>* pState = StateMachine<GameStates>::Instance();
+        pState->GoToState(GameStates::GAME);
+    }
+    else if(event == "WarpEnd")
+    {
+        EventManager* pEvent = EventManager::Instance();
+        pEvent->Fire("WarpBegin", nullptr);
+    }
 }

src/SkyboxSystem.cpp
@@ -18,7 +18,8 @@ using namespace DirectX;
 
 SkyboxSystem::SkyboxSystem()
 {
-	EventManager::Instance()->Register("WarpBegin", this);
+    m_pEvent = EventManager::Instance();
+	m_pEvent->Register("WarpBegin", this);
 	textureNames[0] = "GreenSpace";
 	textureNames[1] = "PurpleSpace";
 	textureNames[2] = "Space";
@@ -80,6 +81,7 @@ void SkyboxSystem::Update(EntityManager* pManager, float dt, float tt)
 		{
 			warping = false;
 			lerpT = 1;
+            m_pEvent->Fire("WarpEnd", nullptr);
 		}
 		else
 		{

commit 4cd55cd2a5632ebd256ae2c24ee4c9b5e67dc098

Fixing memory leak introduced in 4f2dae8.

src/GameState.cpp
@@ -50,10 +50,6 @@ void GameState::Enter(void)
 		camMan->RegisterCamera<Camera>("Main Camera", 0.0f, 0.0f, -5.0f);
 		camMan->SetActiveCamera("Main Camera");
 
-        // Parse "resources.json" for any resources.
-        ResourceManager* pManager = ResourceManager::Instance();
-		pManager->ParseJSONFile("json/resources.json");
-
 		this->LoadCurrentLevel();
 		EventManager* pEventManager = EventManager::Instance();
 		pEventManager->Register("WarpEnd", this);