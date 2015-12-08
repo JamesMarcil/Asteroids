a4688d90778c6f90c8fbcd26c56476326876332b
JamesMarcil <JamesMarcil@users.noreply.github.com>
2015-12-07 23:22:38 -0500

    Tweens class is no longer a Singleton.
    Removed Singleton<T> as the parent of Tweens. Made all methods static, and
    made its constructor, destructor, and assignment operators
    private/deleted.

--- include/Tweens.h
+++ include/Tweens.h
@@ -1,17 +1,24 @@
 #ifndef TWEENS_H
 #define TWEENS_H
 
-#include "Singleton.h"
-
-class Tweens: public Singleton<Tweens>
+class Tweens
 {
+private:
+    // Private/deleted to prevent instantiation.
+    Tweens(void) = delete;
+    ~Tweens(void) = delete;
+    Tweens(const Tweens& rhs) = delete;
+    Tweens& operator=(const Tweens& rhs) = delete;
+    Tweens(Tweens&& rhs) = delete;
+    Tweens& operator=(Tweens&& rhs) = delete;
+
 public:
-	float MapRange(float value, float srcMin, float srcMax, float outMin, float outMax)
+	static float MapRange(float value, float srcMin, float srcMax, float outMin, float outMax)
 	{
 		return (value - srcMin) / (srcMax - srcMin) * (outMax - outMin) + outMin;
 	}
 
-	float EaseInEaseOut(float t)
+	static float EaseInEaseOut(float t)
 	{
 		if (t < 0.5)
 		{
@@ -23,12 +30,12 @@ public:
 		}
 	}
 
-	float EaseIn(float t)
+	static float EaseIn(float t)
 	{
 		return pow(t, 2);
 	}
 
-	float EaseOut(float t)
+	static float EaseOut(float t)
 	{
 		return pow(1 - t, 2);
 	}
